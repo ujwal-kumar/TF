da34ba7b74b7c991c1c914f45fee2bc173f6de2a
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-10-20 15:03:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "aws_sg"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test" {
+  ami = "ami-0912f71e06545ad88"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+  security_groups = ["aws_sg"]
+}
+
+output "aws_instance_public_dns" {
+  value = "${aws_instance.test.public_dns}"
+}
+
+output "aws_instance_public_dns" {
+  value = "${aws_instance.test.public_ip}"
+}
